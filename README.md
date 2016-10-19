ec46d7aa2d441eeb11933e0ec0ca1b11375fe2d0
likeke <likek@outlook.com>
2016-10-20 00:14:18 +0800

    修复重要bug

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # musicVisul
 音乐可视化小练习(使用NodeJS , webAudio API , Canvas)
+# 使用
+1.项目依赖安装完成后，在/public/目录下创建media目录，将音乐文件放置在/public/media目录
+2.运行npm start命令，打开本地3000端口
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -16,23 +16,31 @@ for (var i = 0; i < lis.length; i++) {
 }
 var xhr = new XMLHttpRequest(); //创建xhr对象
 var ac = new(window.AudioContext || window.webkitAudioContext)(); //创建AudioContext
-var gainNode = ac[ac.createGain ? "createGain" : "createGainNode"]();
+var gainNode = ac[ac.createGain ? "createGain" : "createGainNode"](); //创建gainNode对象(主要用来操作音量)
 gainNode.connect(ac.destination);
+var source = null; //存放钱一次播放的资源(解决快速点击时的bug)
+var count = 0; //存放当切换音乐的次数(解决快速点击时的bug)
 /**************************请求发送和请求处理函数*****************************/
 function load(url) {
+    source && source[source.stop ? 'stop' : 'noteOff'](); //前一次先停止
+    var n = ++count; //count是全局变量，因此每次点击都会正常的增加，n为局部变量，每次会深复制一份，有多个n
+    xhr.abort(); //终止前一次xhr请求(万一钱一次请求还未完成时又发生一次点击)
     xhr.open('GET', url);
     xhr.responseType = 'arraybuffer';
     /*××××××××××××××××请求处理完成时××××××××××××××××××*/
     xhr.onload = function () {
+        if (n != count) return; //如果当前load函数内的n和全局的conut不等就return,即只播放最后一次点击的音乐
         /*×××××解析数据×××××*/
         ac.decodeAudioData(xhr.response, function (buffer) {
+            if (n != count) return; //如果不是最后一次点击
             /*解析成功时对数据处理*/
             var bufferSource = ac.createBufferSource();
             bufferSource.buffer = buffer;
-            bufferSource.connect(gainNode);
+            bufferSource.connect(gainNode); //无需再connect到destination
             //            bufferSource.connect(ac.destination);
             /*立即播放*/
             bufferSource[bufferSource.start ? "start" : "noteOn"](0);
+            source = bufferSource;
         }, function (err) {
             /*错误处理*/
             console.log(err);
